10f6ef61d1a9e0f1d3d518eb1f9a31a79eaf1951
Oleksandra1 <oleksandra.tkachenko154.com>
2024-09-29 09:40:01 +0300

    first_adaptive_version

--- index.php
+++ index.php
@@ -321,7 +321,7 @@
       </section>
 
      <!--Collaboration process-->
-      <section id="collaboration" class="collaboration">
+      <section class="collaboration" id="collaboration">
         <div class="container collaboration-container">
           <div class="row">
             <div class="col-12">
@@ -331,7 +331,7 @@
               <canvas id="canvas" width="780"></canvas>
             </div>
           </div>  
-          <div class="row">
+          <div class="row collaboration-items">
             <div class="col-lg-2 col-sm-6 collaboration-item">
               <div class="icon-wrapper">
                 <svg class="collaboration-icon">
@@ -552,6 +552,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     <script src="./js/main.js"></script>
+    <script src="./js/logo-fixed.js"></script>
 </body>
 
 </html>
---
+++ js/logo-fixed.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    var logo = document.querySelector('.logo');
+    var scrollPosition = window.scrollY;
+
+    
+    if (scrollPosition > 50) {
+      logo.classList.add('fixed');
+    } else {
+      logo.classList.remove('fixed');
+    }
+  });
